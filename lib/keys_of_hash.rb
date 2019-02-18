81c4ba22f7ab367268d6cfbb26b1ba0054f074d9
Vytautas Klimavicius <vyklimavicius@hotmail.com>
2019-02-18 16:42:57 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
+# animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama",
+# "kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*argument)
     # code goes here
+    # new_array = Array.new
+    map do |key,value|
+      if argument.include?(value)
+         key
+      end
+    end.compact
   end
 end
+
+
+
+
+
+
+# p animals.keys_of(animals,"Panama")
--- spec/keys_of_hash_spec.rb
+++ spec/keys_of_hash_spec.rb
@@ -1,7 +1,7 @@
 describe 'keys_of' do
 
   let(:animals) { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
-    
+
   it "returns an array" do
     expect(animals.keys_of('Panama').class).to eq(Array)
   end
